b080056f093566d6e524c601721fe5038dbc07a1
RkEclair <ryosukadnak@gmail.com>
2019-04-27 01:51:20 +0900

    Impl members page

--- pages/members.js
+++ pages/members.js
@@ -1,9 +1,56 @@
+const Member = ({info}) => {
+  const {name, job, twitter, youtube, github, favorite, description} = info;
+  return (
+    <div
+      key={name}
+      style={{
+        border: '3px solid lightgray',
+        padding: 5,
+        margin: '10px 0',
+        borderRadius: 5
+      }}
+    >
+      <div style={{fontSize: '1.2em', fontWeight: 'bold'}}>{name}</div>
+      {twitter && <a href={twitter}>Twitter</a>}
+      {youtube && <a href={youtube}>YouTube</a>}
+      {github && <a href={github}>Github</a>}
+      <div>{`Job: ${job}`}</div>
+      <div>{`Favorite: ${favorite}`}</div>
+      <div style={{fontSize: '0.8em', textIndent: '1em'}}>{description}</div>
+      <style jsx>{`
+        a {
+          text-decoration: none;
+          color: darkcyan;
+          margin-left: 1em;
+        }
+        a:hover {
+          text-decoration: underline;
+        }
+      `}</style>
+    </div>
+  );
+};
+
 import Header from '../comps/header';
 
-export default () => (
+const Members = ({members}) => (
   <div>
     <Header />
     <h1>We are VTuber Peogrammer's Network</h1>
-    <div></div>
+    <div>
+      {members.map((e) => (
+        <Member info={e} />
+      ))}
+    </div>
   </div>
 );
+
+import fetch from 'isomorphic-unfetch';
+
+Members.getInitialProps = async () => {
+  const res = await fetch('http://localhost:3000/static/members.json');
+  const json = await res.json();
+  return {members: json.members};
+};
+
+export default Members;
